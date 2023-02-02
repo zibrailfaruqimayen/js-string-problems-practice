139bbdc9902ea37e877ad896bfc89d0e8809b8e5
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-02 22:37:54 +0600

    fibo

---
+++ max.js
@@ -0,0 +1,28 @@
+// problem 1
+const jim = 84;
+const dela = 75;
+if(jim > dela){
+    console.log('jim will get the cake');
+}
+else{
+    console.log('dela you will get the cake')
+}
+
+// Quiz problem 5
+const number = -78; 
+const answer = Math.abs(number); 
+console.log(answer);
+
+// Quiz problem 8
+
+var a=2; 
+var b=3; 
+if(a < b){
+	console.log("Hello");
+}
+
+// Quiz problem 10
+function add(a, b){
+    return a + b;
+  }
+  console.log(add("adam" + "eve"))
---
+++ max3.js
@@ -0,0 +1,33 @@
+// problem 2
+// --------------------
+// const jim = 83;
+// const dela = 99;
+// const chinku = 77;
+
+// if(jim > dela && jim > chinku){
+//     console.log('jim will get the cake');
+// }
+// else if (dela > jim && dela > chinku){
+//     console.log('dela will get the cake');
+// }
+// else{
+//     console.log('chinku will get the cake');
+// }
+// -----------------------------------------------
+
+// problem 3
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if(jim > dela && jim > chinku){
+    console.log('jim will get the cake');
+}
+else if (dela > jim && dela > chinku){
+    console.log('dela will get the cake');
+}
+else{
+    console.log('chinku will get the cake');
+}
+
+Math.max(12, 45);
